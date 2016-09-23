9b368579f3f41fa946cb974f56186090879f25f9
Parham Alvani <parham.alvani@gmail.com>
2016-09-24 00:04:42 +0330

    Horrible question was added.

---
+++ 2016/c-quizz.c
@@ -0,0 +1,21 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : c-quizz.c
+ *
+ * [] Creation Date : 23-09-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+ */
+/*
+ * Copyright (c) 2016 Parham Alvani.
+ */
+#include <stdio.h>
+
+int v, i, j, k, l, s, a[99];
+
+int main(){
+	for(scanf("%d", &s); *a - s; v = a[j *= v] - a[i], k = i < s, j += (v = j < s && (!k && !!printf(2 + "\n\n%c" - (!l << !j), " #Q"[l^v?(l^j)&1:2])&&++l||a[i]<s && v && v - i + j && v + i - j)) && !(l %= s), v || (i == j ? a[i +=k ] = 0: ++a[i]) >= s * k && ++a[--i]);
+}
+
