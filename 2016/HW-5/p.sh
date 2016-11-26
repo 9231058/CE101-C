c747914ffed683990250f05fc273ce25e8d74f07
Parham Alvani <parham.alvani@gmail.com>
2016-11-26 09:38:23 +0330

    Problem creation script was added.

---
+++ 2016/HW-5/p.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : p.sh
+#
+# [] Creation Date : 04-11-2015
+#
+# [] Created By : Elahe Jalalpour (el.jalalpour@gmail.com)
+# =======================================
+
+
+counter=1
+for i in "${1}/in/"*.txt; do
+	output=$(printf "output%d.txt" ${counter})
+	go run ${1}.go < "${i}" > "${1}/out/${output}"
+	echo "Problem ${1} - Test ${counter}: ${i} --> ${output}"
+	let counter=counter+1
+done
+
+cd ${1}; zip -r "../${1}" *; cd ..
+echo "Zip file created for quera..."
