b7a22598a9ba9af7e6ce2290202a9a65c98d5855
Parham Alvani <parham.alvani@gmail.com>
2016-11-29 01:44:14 +0330

    C base problem creation was added.

---
+++ 2016/HW-5/p-c.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : p-c.sh
+#
+# [] Creation Date : 04-11-2015
+#
+# [] Created By : Elahe Jalalpour (el.jalalpour@gmail.com)
+# =======================================
+
+echo "Compiling..."
+gcc -o ${1}.out -Wall ${1}.c -lm
+echo "Compilation was successful"
+
+counter=1
+for i in "${1}/in/"*.txt; do
+	output=$(printf "output%d.txt" ${counter})
+	./${1}.out < "${i}" > "${1}/out/${output}"
+	echo "Problem ${1} - Test ${counter}: ${i} --> ${output}"
+	let counter=counter+1
+done
+
+cd ${1}; zip -r ../${1} *; cd ..
+echo "Zip file created for quera..."
