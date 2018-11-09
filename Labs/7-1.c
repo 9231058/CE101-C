e43de9e69e14ba39ac94c4e32c87779ffb292dd6
Parham Alvani <parham.alvani@gmail.com>
2018-11-09 22:27:17 +0330

    Improves seesion 7 manual.

--- Labs/7-1.c
+++ Labs/7-1.c
@@ -7,17 +7,27 @@
  *
  * [] Created By : Parham Alvani <parham.alvani@gmail.com>
  * =======================================
-*/
+ */
 /*
-* Copyright (c)  2018 Parham Alvani.
-*/
+ * Copyright (c)  2018 Parham Alvani.
+ */
 #include <stdio.h>
+#include <math.h>
+
+#define awesome pow(pow(3.0, 0.05), 20.0);
 
 int main() {
-  double d;
-  scanf("%lf", &d);
+  double d = awesome;
 
-  if ((int) d == d) { // d is an integer
+  printf("Method 1 -- based on equality\n");
+  if (lround(d) == d) {
+    printf("%lf is an integer\n", d);
+  } else {
+    printf("%lf isn't an integer (%d)\n", d, (int)d);
+  }
+
+  printf("Method 2 -- based on error rage\n");
+  if (fabs(lround(d) - d) <= 0.000001) { // d is an integer
     printf("%lf is an integer\n", d);
   } else {
     printf("%lf isn't an integer (%d)\n", d, (int)d);
