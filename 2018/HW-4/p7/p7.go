d41ede8d867aabc20cec439c4bbd7f337f439aad
Parham Alvani <parham.alvani@gmail.com>
2018-11-16 00:19:29 +0330

    Adds problem 7 implementation.

---
+++ 2018/HW-4/p7/p7.go
@@ -0,0 +1,37 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 13-11-2018
+ * |
+ * | File Name:     750A.go
+ * +===============================================
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	var n, k int
+
+	fmt.Scanf("%d %d", &n, &k)
+
+	contestTime := k
+	start := 0
+	end := n
+
+	for end >= start {
+		i := (start + end) / 2
+		if 5*i*(i+1)/2 > contestTime {
+			end = i - 1
+		} else {
+			start = i + 1
+		}
+	}
+
+	fmt.Println(end)
+}
