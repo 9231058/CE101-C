b593251d603c496dd48e89708de57d868fd01b98
Parham Alvani <parham.alvani@gmail.com>
2018-11-30 22:48:48 +0330

    Adds materials of workshop's 10th session.

---
+++ Labs/10-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[]) {
+    int n;
+    scanf("%d", &n);
+
+    int D[n];
+    
+    D[0] = 1; // f(0) = 1, fibonacci sequence number is 1
+    D[1] = 1; // f(1) = 1, fibonnaci sequence number is 2
+    for (int i = 2; i < n; i++) {
+        D[i] = D[i - 1] + D[i - 2];
+    }
+    printf("%d\n", D[n - 1]);
+}
---
+++ Labs/10-4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int CheckString(char input[], char name[]);
+
+void bubbleSort(char names[][], int location[]);
+
+void main()
+{
+    int n;
+    char names[100];
+    char strs[100][100];
+    int location[100];
+    //Write your code here!
+}
+
+int CheckString(char input[], char name[])
+{
+    //Write your code here!
+    //Hint: you can return -1 if the input string does not contain the name  string
+}
+
+void bubbleSort(char names[][], int location[])
+{
+    //Write your code here!
+}
