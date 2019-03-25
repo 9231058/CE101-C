e19f267f0a5226e5829633badae93e91978a0d8b
Parham Alvani <parham.alvani@gmail.com>
2019-03-25 14:03:25 +0430

    Corrects and issue with homework-2 problem-4.

--- 2019/HW-2/p4/p4.go
+++ 2019/HW-2/p4/p4.go
@@ -7,5 +7,11 @@ func main() {
 
 	fmt.Scanf("%d %d %d %d %d", &x, &s1, &m1, &s2, &m2)
 
-	fmt.Println(x - (s2*60+m2-(s1*60+m1))%x)
+	diff := (s2*60 + m2 - (s1*60 + m1))
+
+	if diff%x == 0 {
+		fmt.Println(0)
+	} else {
+		fmt.Println(x - diff%x)
+	}
 }
