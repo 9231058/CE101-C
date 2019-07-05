e7b421aa53ea7c0d0d37d6c5361452a1c1126ca7
Parham Alvani <parham.alvani@gmail.com>
2019-07-05 12:24:04 +0430

    feat: Move Problem Script into Scripts Folder

--- scripts/p.sh
+++ scripts/p.sh
@@ -12,6 +12,7 @@
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 usage() {
+        echo "this script create the zip folder for the given problem that can be uploaded to Quera website"
         echo "$0 [hw-?] [p?]"
 }
 
@@ -22,11 +23,11 @@ fi
 
 hw="HW-$1"
 p="p$2"
-tc="$current_dir/$hw/$p/tc"
-go="$current_dir/$hw/$p/$p.go"
+tc="$current_dir/$hw/$p/tc"    # problem test cases directory
+go="$current_dir/$hw/$p/$p.go" # problem solution in Go
 
 if ! [ -d $tc ]; then
-        echo "There is no test case in $tc"
+        echo "there is no test case folder in $tc"
         exit
 fi
 
@@ -34,7 +35,7 @@ fi
 # output files
 if ! [ -f $tc/tester.cpp ]; then
         if ! [ -f $go ]; then
-                echo "There is no go source in $go"
+                echo "there is no go source in $go"
                 exit
         fi
 
@@ -43,7 +44,7 @@ if ! [ -f $tc/tester.cpp ]; then
                 OIFS=$IFS
                 IFS=';' # split output based on ';' instead of newline
                 i=0
-                echo "[beta] Generates test case based on generator.go"
+                echo "[beta] generates test case based on generator.go"
 
                 start=$(date +'%s')
 
@@ -51,13 +52,13 @@ if ! [ -f $tc/tester.cpp ]; then
                         i=$(( i + 1 ))
                         echo $input > "$tc/in/input$i.txt"
 
-                        echo "Case $i: $tc/in/input$i.txt"
+                        echo "case $i: $tc/in/input$i.txt"
                         echo
                 done
                 IFS=$OIFS
 
                 took=$(( $(date +'%s') - $start ))
-                printf "$tc/generator.go Took %ds.\n" $took
+                printf "$tc/generator.go took %ds.\n" $took
         fi
 
         for input in "$tc/in/"*.txt; do
@@ -70,14 +71,14 @@ if ! [ -f $tc/tester.cpp ]; then
                 go run $go < $input > "$tc/out/output$i.txt"
 
                 took=$(( $(date +'%s') - $start ))
-                printf "$go Took %ds.\n" $took
+                printf "$go took %ds.\n" $took
 
-                echo "Test $i: $input --> $tc/out/output$i.txt"
+                echo "test $i: $input --> $tc/out/output$i.txt"
                 echo
         done
 else
-        echo "You are using Quera tester.cpp, please make sure you know what you are doing"
+        echo "you are using Quera tester.cpp, please make sure you know what you are doing"
 fi
 
 cd $tc && zip -r "$current_dir/$hw-$p.zip" *; cd -
-echo "Quera TC zip is ready"
+echo "quera TC zip is ready"
