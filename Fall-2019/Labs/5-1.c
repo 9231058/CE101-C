0def850bb97fbda76256593f1e9fe490037e0f16
Parham Alvani <parham.alvani@gmail.com>
2019-10-28 21:55:30 +0330

    feat: Add Workshop 5 Problem 1 Solution

---
+++ Fall-2019/Labs/5-1.c
@@ -0,0 +1,22 @@
+/*
+ * In The Name of God
+ * =======================================
+ * [] File Name : 5-1.c
+ *
+ * [] Creation Date : 28-10-2019
+ *
+ * [] Created By : Parham Alvani <parham.alvani@gmail.com>
+ * =======================================
+*/
+/*
+* Copyright (c)  2019 Parham Alvani.
+*/
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+  double b, c;
+  scanf("%lf %lf", &b, &c);
+
+  printf("%lf\n", sqrt(b * b + c * c));
+}
