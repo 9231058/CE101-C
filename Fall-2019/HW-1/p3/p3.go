2e14edd34c9523192532d9322f6c7c1e85e11068
Parham Alvani <parham.alvani@gmail.com>
2019-10-29 19:35:17 +0330

    feat: Add Problem 3 Solution

---
+++ Fall-2019/HW-1/p3/p3.go
@@ -0,0 +1,32 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 29-10-2019
+ * |
+ * | File Name:     p3.go
+ * +===============================================
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+
+	if _, err := fmt.Scanf("%d", &n); err != nil {
+		return
+	}
+
+	m := 0
+	for n != 0 {
+		m = m*10 + n%10
+		n = n / 10
+	}
+
+	fmt.Println(m)
+}
