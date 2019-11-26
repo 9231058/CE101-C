26bd5ca27eff134f4326edf5474604c1c2e1b2b8
Parham Alvani <parham.alvani@gmail.com>
2019-11-26 20:59:53 +0330

    feat: Add Input Handling

--- Fall-2018/Midterm/p3.c
+++ Fall-2018/Midterm/p3.c
@@ -5,6 +5,18 @@ int check2(int, int, int, int, int, int, int, int);
 int distance2(int, int, int, int);
 
 int main() {
+  int x1, y1;
+  int x2, y2;
+  int x3, y3;
+  int x4, y4;
+
+  scanf("%d %d", &x1, &y1);
+  scanf("%d %d", &x2, &y2);
+  scanf("%d %d", &x3, &y3);
+  scanf("%d %d", &x4, &y4);
+
+  printf("check 1: %d\n", check1(x1, y1, x2, y2, x3, y3, x4, y4));
+  printf("check 2: %d\n", check2(x1, y1, x2, y2, x3, y3, x4, y4));
 }
 
 int distance2(int x1, int y1, int x2, int y2) {
