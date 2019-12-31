fc614210c132f8a111897e9a59fbd55e482154ad
Parham Alvani <parham.alvani@gmail.com>
2019-12-31 12:30:02 +0330

    feat: Complete Problem 3

--- Fall-2019/HW-8/p3/p3.go
+++ Fall-2019/HW-8/p3/p3.go
@@ -1,8 +1,40 @@
 package main
 
+import "fmt"
+
+// ListNode is a linked-list node
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func main() {
+	var l1, p1 *ListNode
+
+	for {
+		var n int
+		if _, err := fmt.Scanf("%d", &n); err != nil {
+			return
+		}
+		if n == 0 {
+			break
+		}
+		if l1 == nil {
+			p1 = &ListNode{
+				Val:  n,
+				Next: nil,
+			}
+			l1 = p1
+		} else {
+			p1.Next = &ListNode{
+				Val:  n,
+				Next: nil,
+			}
+			p1 = p1.Next
+		}
+	}
+
+	fmt.Println(isPalindrome(l1))
 }
 
 func isPalindrome(head *ListNode) bool {
